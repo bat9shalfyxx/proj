b3a2b17a5a6f005b8e7d8f3a58bf4aff9d2cf051
bat9shalfyxx <shokirovttt192@gmail.com>
2025-09-21 00:14:29 +0400

    дописал логику регистарционного окна

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,38 +1,65 @@
-// const regButton = document.querySelector(".header-right-profile-button");
+document.addEventListener("DOMContentLoaded", () => {
+    const regButton = document.querySelector(".header-right-profile-button");
+    const mainContent = document.querySelector(".switch-content");
+    const regContent = document.querySelector(".registration");
 
-// regButton.addEventListener("click", (e) => {
-//     e.preventDefault();
+    regButton.addEventListener("click", (e) => {
+        e.preventDefault();
 
-//     regButton.classList.add("active");
-// });
+        if (regContent.hasAttribute(".active")) {
+            console.log(q);
+        }
 
+        if (e.target === regButton && !regContent.classList.contains("active")) {
+            regContent.classList.add("active");
+            mainContent.style.display = "none";
+            regContent.style.display = "grid";
+        } else {
+            regContent.classList.remove("active");
+            regContent.style.display = "none";
+            mainContent.style.display = "block";
+        }
+    });
+});
+
+//// ВОЙТИ - ЗАРЕГИСТРИРОВАТЬСЯ
 document.addEventListener("DOMContentLoaded", () => {
     const signIn = document.querySelector(".sign-in");
     const signUp = document.querySelector(".sign-up");
     const signInForm = document.querySelector(".sign-in-form");
     const signUpForm = document.querySelector(".sign-up-form");
+    const regBlock = document.querySelector(".registration-block");
 
     signUpForm.style.display = "none";
 
-    if (signIn.hasAttribute(".active")) {
-        signInForm.style.display = "grid";
-        signUpForm.style.display = "none";
-    }
-
     signIn.addEventListener("click", (e) => {
+        e.preventDefault();
+
         if (e.target === signIn && !signIn.hasAttribute(".active")) {
+            regBlock.classList.add("reg-sign-in");
+            regBlock.classList.remove("reg-sign-up");
+
             signIn.classList.add("active");
             signUp.classList.remove("active");
 
+            signIn.style.color = "black";
+            signUp.style.color = "var(--dark-gray-col)";
             signInForm.style.display = "grid";
             signUpForm.style.display = "none";
         }
     });
+
     signUp.addEventListener("click", (e) => {
-        if (e.target === signUp && !signUp.hasAttribute("active")) {
+        e.preventDefault();
+        if (e.target === signUp && !signUp.hasAttribute(".active")) {
+            regBlock.classList.remove("reg-sign-in");
+            regBlock.classList.add("reg-sign-up");
+
             signUp.classList.add("active");
             signIn.classList.remove("active");
 
+            signUp.style.color = "black";
+            signIn.style.color = "var(--dark-gray-col)";
             signInForm.style.display = "none";
             signUpForm.style.display = "grid";
         }
