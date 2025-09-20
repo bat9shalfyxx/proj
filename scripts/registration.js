a055d0892ddde63a44e7dbfad9c11c14fdfba972
bat9shalfyxx <shokirovttt192@gmail.com>
2025-09-20 21:01:45 +0400

    переключение между входом и регистрацией

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,7 +1,40 @@
-const regButton = document.querySelector(".header-right-profile-button");
+// const regButton = document.querySelector(".header-right-profile-button");
 
-regButton.addEventListener("click", (e) => {
-    e.preventDefault();
+// regButton.addEventListener("click", (e) => {
+//     e.preventDefault();
 
-    regButton.classList.add("active");
+//     regButton.classList.add("active");
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const signIn = document.querySelector(".sign-in");
+    const signUp = document.querySelector(".sign-up");
+    const signInForm = document.querySelector(".sign-in-form");
+    const signUpForm = document.querySelector(".sign-up-form");
+
+    signUpForm.style.display = "none";
+
+    if (signIn.hasAttribute(".active")) {
+        signInForm.style.display = "grid";
+        signUpForm.style.display = "none";
+    }
+
+    signIn.addEventListener("click", (e) => {
+        if (e.target === signIn && !signIn.hasAttribute(".active")) {
+            signIn.classList.add("active");
+            signUp.classList.remove("active");
+
+            signInForm.style.display = "grid";
+            signUpForm.style.display = "none";
+        }
+    });
+    signUp.addEventListener("click", (e) => {
+        if (e.target === signUp && !signUp.hasAttribute("active")) {
+            signUp.classList.add("active");
+            signIn.classList.remove("active");
+
+            signInForm.style.display = "none";
+            signUpForm.style.display = "grid";
+        }
+    });
 });
