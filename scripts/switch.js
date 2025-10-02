5d60d486aa4a88e9ff7be0e0f52a2142355df289
bat9shalfyxx <shokirovttt192@gmail.com>
2025-10-02 13:09:56 +0400

    доп. адавтив

--- scripts/switch.js
+++ scripts/switch.js
@@ -6,8 +6,8 @@ const hubIcon = document.querySelector(".header-left-logo");
 const profileIcon = document.querySelector(".header-right-profile-logo");
 const requestButton = document.querySelector(".header-right-info");
 
-hubPage.style.display = "none";
-profilePage.style.display = "block";
+hubPage.style.display = "block";
+profilePage.style.display = "none";
 
 hubIcon.addEventListener("click", (e) => {
     e.preventDefault();
