589a101f8fe537e7afd2ae968ce73d3ed0dd88dc
bat9shalfyxx <shokirovttt192@gmail.com>
2025-10-08 02:24:54 +0400

    nothing

---
+++ scripts/hiddenNav.js
@@ -0,0 +1,7 @@
+const hiddenNavBtn = document.querySelector(".hidden-nav-btn");
+console.log(header.offsetHeight);
+
+hiddenNavBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(1);
+});
